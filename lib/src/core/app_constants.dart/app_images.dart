0a809c30ccf8dae8d864504f34511850cf1d580e
Ahmed Tarek <ahmedtsalem9@gmail.com>
2024-12-04 13:07:37 +0200

    feat: Add assets

---
+++ lib/src/core/app_constants.dart/app_images.dart
@@ -0,0 +1,6 @@
+abstract class AppImages {
+  static const path = 'assets/images/';
+  static const logo = '${path}logo.png';
+  static const searchBackground = '${path}search_background.png';
+  static const homeBackground = '${path}home_background.png';
+}
